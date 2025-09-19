7e167b00eb3683b84192220e92e297dbe9ea466b
Reagan Spurlock <07spree@gmail.com>
2025-09-19 08:22:40 -0400

    fixed my gitignore so i dont violate ndas

---
+++ website-ftr/client/clientapp/src/Components/OfficerPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HeaderDropdown } from '../Stylesheets/mainStyles';
+
+export const OfficerPage = () => {
+
+
+    
+
+    return (
+        <div>
+            <title>Our Officers</title>
+            <HeaderDropdown />
+
+        </div>
+    )
+};
