0e1a69316fc03642c5f89cf93d681b05e78e3904
Reagan Spurlock <07spree@gmail.com>
2025-11-27 22:41:30 -0500

    bug fixes to createart.tsx

--- website-ftr/client/clientapp/tailwind.config.js
+++ website-ftr/client/clientapp/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'times': ['"Times New Roman"', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
